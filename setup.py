11d6b394c5d045f52bad4b3140cd1381ca54548a
Phivos Stylianides <stphivos@gmail.com>
2017-11-08 19:38:13 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='bg-kube',
+    packages=['src'],
+    version='0.0.1',
+    description='An interface that uses gcloud and kubectl to achieve blue-green deployments to GKE',
+    author='Phivos Stylianides',
+    author_email='stphivos@gmail.com',
+    url='https://github.com/stphivos/bg-kube',
+    keywords=['kubernetes', 'blue-green'],
+    classifiers=[],
+    install_requires=[],
+    entry_points={'console_scripts': ['bg-kube = src.__main__:run', ], },
+)
