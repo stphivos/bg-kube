c216cfc2faa86e1bd0abc4354c25169e1c3955f4
Phivos Stylianides <stphivos@gmail.com>
2017-11-18 20:13:28 +0200

    Update version to v0.0.10

--- setup.py
+++ setup.py
@@ -4,15 +4,22 @@ from pip.req import parse_requirements
 install_req = parse_requirements('requirements/core.txt', session='skip')
 req = [str(ir.req) for ir in install_req]
 
+
+def read(filename):
+    with open(filename) as file:
+        return file.read()
+
+
 setup(
     name='bg-kube',
     packages=['bgkube'],
-    version='0.0.9',
+    version='0.0.10',
     description='An interface for automating blue-green deployments using Kubernetes',
+    long_description=read('README.rst'),
     author='Phivos Stylianides',
     author_email='stphivos@gmail.com',
     url='https://github.com/stphivos/bg-kube',
-    keywords=['kubernetes', 'blue-green deployments', 'google cloud', 'ci/cd', 'continuous deployment'],
+    keywords='Kubernetes blue-green GKE AWS Kops CI/CD Continuous deployment',
     classifiers=[],
     install_requires=req,
     entry_points={'console_scripts': ['bg-kube = bgkube.__main__:run', ], },
