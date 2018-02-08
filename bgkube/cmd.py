793bcc1691ce3c7a8a66d2ce5357224f9b798b0b
Phivos Stylianides <stphivos@gmail.com>
2018-02-08 17:28:16 +0100

    Add support for minikube with local docker registry

--- bgkube/cmd.py
+++ bgkube/cmd.py
@@ -2,7 +2,7 @@
 DOCKER_BUILD = 'docker build {context} -f {dockerfile} -t {image}:{tag}'
 
 # docker-machine
-DOCKERMACHINE_EVAL_ENV = 'which docker-machine && eval $(docker-machine env {})'
+DOCKERMACHINE_ENV = 'which docker-machine && eval $(docker-machine env {})'
 
 # gcloud
 GCLOUD_CONTAINER_CLUSTER_GET_CREDENTIALS = 'gcloud container clusters get-credentials {cluster} --zone {zone}'
@@ -15,3 +15,8 @@ AWS_DOCKER_PUSH = 'docker push {}'
 
 # kubectl
 KUBECTL_EXEC = 'kubectl exec {pod} {command} {args}'
+KUBECTL_CONFIG_MINIKUBE = 'kubectl config use-context minikube'
+
+# minikube
+MINIKUBE_SERVICE_URL = 'minikube service {} --url'
+MINIKUBE_DOCKER_ENV = 'eval $(minikube docker-env)'
